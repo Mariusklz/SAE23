bcbb441fb0161043f604b586c3301bc8586fdcf1
AkifLeBogossSelonMaman <akifaslankilic2005@gmail.com>
2024-05-16 09:51:43 +0200

    16/05/2024

--- project_sae/filmographie/models.py
+++ project_sae/filmographie/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
+class CustomUser(AbstractUser):
+    TYPE_CHOICES = (
+        ('professionnel', 'Professionnel'),
+        ('amateur', 'Amateur'),
+    )
+
+    pseudo = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return self.pseudo, self.type
